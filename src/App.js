21ae561d7532e7efd58a7523509d55e396fe7a33
A12h16 <anushadevaiah1999@gmail.com>
2023-01-21 00:00:01 +0530

    feat: :art: Multiple components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import HomePage from './Home';
+import Navbar from './Navbar';
 
 function App() {
   const title ="Anusha's world"
   return (
     <div className="App">
+      <Navbar/>
      <div className="Container">
-      <h1>{title}</h1>
+      <HomePage/>
      </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navabr">
+            <h1>Anusha blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+             \
+                <a href="/craete">New blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
