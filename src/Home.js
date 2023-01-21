642f9a062da2458b6dd8450ae2c142bdb9d41687
A12h16 <anushadevaiah1999@gmail.com>
2023-01-21 10:39:39 +0530

    feat: :art: Click events and functions

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
 const HomePage = () => {
+
+    const handelClick =()=>{
+        console.log("I am clicked")
+    }
+
+    const handelClickWithArg =(name)=>{
+        console.log("hello " + name)
+    }
+     
+    const handelClickForEvent =(name,e)=>{
+        console.log("hello " + name ,e.target)
+    }
+
     return ( 
         <div className="home">
             <h1>Home Page</h1>
+            <button onClick={handelClick}>Click me</button>
+            <button onClick={()=>handelClickWithArg("anusha")}>Click me again</button>
+            <button onClick={(e)=>handelClickForEvent("anusha",e)}>Click me again for event</button>
         </div>
      );
 }
