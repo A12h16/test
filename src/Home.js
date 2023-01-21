6c38717fc747bfaaaf95ea90f83342e9f27799c3
A12h16 <anushadevaiah1999@gmail.com>
2023-01-21 11:26:31 +0530

    feat: :fire: Displaying/outputting list using ,map function

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,23 @@
 import { useState } from "react";
 
 const HomePage = () => {
-    const [name,setName]=useState("anusha");
-    const [age,setAge]=useState(18);
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
-    const handelClick =()=>{
-        setName("Bojamma");
-        setAge(24)
-    }
 
-    return ( 
+    return (
         <div className="home">
-            <h1>Home Page</h1>
-            <p>{name} is {age} old</p>
-            <button onClick={handelClick}>Click me</button>
+            {blogs.map((blog) =>
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>)
+            }
         </div>
-     );
+    );
 }
- 
-export default HomePage ;
+
+export default HomePage;
