85f5f3e58d8fd7e00745a01eec957d1abfeaa241
A12h16 <anushadevaiah1999@gmail.com>
2023-01-22 18:15:12 +0530

    fix:

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,12 @@
+import {Link} from "react-router-dom"
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Anusha blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/craete" style={{
-                    color:"white",
-                    backgroundColor:"#f1356d",
-                    borderRadius:"8px"
-                }}>New blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New blog</Link>
             </div>
         </nav>
      );
