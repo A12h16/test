a664c9278671ee64369432e6d6db6d3af5a2e14d
A12h16 <anushadevaiah1999@gmail.com>
2023-01-22 12:57:19 +0530

    feat: :zap: useEffect Hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import BlogList  from "./BlogList"
 
 const HomePage = () => {
@@ -13,6 +13,11 @@ const handelDelete=(id)=>{
  const newBlog= blogs.filter(blog =>blog.id!==id)
  setBlogs(newBlog)
 }
+
+useEffect(()=>{
+    console.log("useEffect here")
+})
+
 
     return (
         <div className="home">
