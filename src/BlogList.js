521e430a225c438d69f4b0b9857bf6399000ef1b
A12h16 <anushadevaiah1999@gmail.com>
2023-01-22 11:39:04 +0530

    fix: :zap: passing function as props
    passing function as a blog

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs,title}) => {
+const BlogList = ({blogs,title,handelDelete}) => {
    // const {blogs,title}=props
     return ( 
         <div className="blog-list">
@@ -7,6 +7,7 @@ const BlogList = ({blogs,title}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={()=>handelDelete(blog.id)}>Delete Blog</button>
                 </div>)
             }
         </div>
--- src/Home.js
+++ src/Home.js
@@ -9,11 +9,14 @@ const HomePage = () => {
     ])
 
 
+const handelDelete=(id)=>{
+ const newBlog= blogs.filter(blog =>blog.id!==id)
+ setBlogs(newBlog)
+}
+
     return (
         <div className="home">
-          <BlogList blogs={blogs} title="All Blogs"/>  
-
-          <BlogList blogs={blogs.filter((blog)=>blog.author==="mario")} title="Filtered Blogs"/>  
+          <BlogList blogs={blogs} title="All Blogs" handelDelete={handelDelete}/>  
         </div>
     );
 }
